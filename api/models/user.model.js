c0c315ec7633ef980871b05efc702ea7e9efdb1f
nishanth-s03 <nishubangerasz@gmail.com>
2023-12-23 20:37:11 +0530

    Created & Adde: User.Model Schema Created And Added!!

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,5 +20,5 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 export default User;
