86201186918e3fbb33411566210abb7624653218
nishanth-s03 <nishubangerasz@gmail.com>
2023-12-23 19:59:13 +0530

    Created: Connected With MongoDB Server

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import { mongoose } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_CONN)
+  .then(() => {
+    console.log("Connected to MongoDB !!");
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
 
 const app = express();
-
 app.listen(3000, () => {
   console.log("Server Running On PORT:3000 !!");
 });
