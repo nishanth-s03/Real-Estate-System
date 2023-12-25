783ce0128f416a7e132ada17797414ad80682eb5
nishanth-s03 <nishubangerasz@gmail.com>
2023-12-25 21:21:46 +0530

    Updated: Signup Page Upadated!!!

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 export default function SignUp() {
     return (
-        <div>
-            SignUp
+        <div className="p-3 max-w-lg mx-auto">
+            <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+            <form className="flex flex-col gap-4">
+                <input type="text" 
+                placeholder='Username..'
+                className="border p-3 rounded-lg"
+                id="username" />
+                <input type="email" 
+                placeholder='Email ID..'
+                className="border p-3 rounded-lg"
+                id="email" />
+                <input type="password" 
+                placeholder='Password..'
+                className="border p-3 rounded-lg"
+                id="password" />
+                <button
+                    className="bg-slate-700 text-white p-3 rounded-lg 
+                    hover:opacity-95
+                    disabled:opacity-80"
+                >SIGN UP</button>
+            </form>
+            <div className="flex gap-2 mt-5">
+                <p>Already have an account?</p>
+                <Link to={"/sign-in"}>
+                    <span className='text-blue-600'>Sign In</span>
+                </Link>
+            </div>
         </div>
     )
 }
