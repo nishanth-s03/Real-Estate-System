bcb8fdaf45239d67065936a6094a1ea641610941
nishanth-s03 <nishubangerasz@gmail.com>
2023-12-26 19:48:15 +0530

    Added: User Signup functionality!!!

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.listen(3000, () => {
 app.use('/api/user', UserRouter);
 app.use('/api/auth', AuthRouter);
 
-app.use((err,req,next) => {
+app.use((err,req,res,next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
@@ -32,3 +32,14 @@ app.use((err,req,next) => {
     message,
   });
 });
+
+
+
+
+
+
+
+
+
+
+
