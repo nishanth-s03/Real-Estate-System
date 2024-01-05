b2041bff25966a557a885fcf7030956594298d4e
nishanth-s03 <nishubangerasz@gmail.com>
2024-01-05 22:14:44 +0530

    Completed: view profileUser Authentication!!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRouter';
 
 export default function App() {
   return(
@@ -17,7 +18,9 @@ export default function App() {
         <Route path="/sign-in"element={<SignIn />}/>
         <Route path="/sign-up"element={<SignUp />}/>
         <Route path="/about"element={<About />}/>
-        <Route path="/profile"element={<Profile />}/>
+        <Route element={<PrivateRoute/>}>
+          <Route path="/profile"element={<Profile />}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   </div>
