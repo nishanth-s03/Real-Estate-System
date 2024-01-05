b61093437dfbce783c67d9f45a9540e1c598bef1
nishanth-s03 <nishubangerasz@gmail.com>
2024-01-05 21:41:33 +0530

    Completed: Google OAuth for User Authentication!!

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar:{
+      type: String,
+      default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonduck.com%2Ficons%2F180867%2Fprofile-circle&psig=AOvVaw3D3ty0hQjESIMcrLejdual&ust=1704556679971000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCJj8j9TOxoMDFQAAAAAdAAAAABAD",
+    },
   },
   { timestamps: true }
 );
