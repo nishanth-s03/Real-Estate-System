968a29736d3f91c9d1c38bdf5a8a765da31aea83
nishanth-s03 <nishubangerasz@gmail.com>
2024-01-22 21:09:30 +0530

    Created: Create Listimg Page Created!!

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function CreateListing() {
   return (
-    <main className="p-3 max-w-xl mx-auto">
+    <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold text-center my-7">Create Listing</h1>
-      <form className="flex flex-col sm:flex-row">
-        <div className="flex flex-col gap-4 flex-1">
+      <form className="flex flex-col sm:flex-row gap-4">
+        <div className="flex flex-col flex-1 gap-4">
           <input
             type="text"
             placeholder="Name"
@@ -20,6 +20,7 @@ export default function CreateListing() {
             placeholder="Description"
             className=" border p-3 rounded-lg"
             id="description"
+            rows="4"
             maxLength="250"
             minLength="10"
             required
@@ -53,7 +54,7 @@ export default function CreateListing() {
               <span className="text-sm font-semibold">Offer</span>
             </div>
           </div>
-          <div>
+          <div className="flex flex-wrap gap-6">
             <div className="flex items-center gap-2">
               <input
                 type="number"
@@ -81,22 +82,51 @@ export default function CreateListing() {
                 type="number"
                 className="p-3 border border-gray-300 rounded-lg"
                 id="regularPrice"
+                min="1"
+                max="10"
                 required
               />
-              <p className="font-semibold">Regular Price</p>
+              <div className="flex flex-col items-center">
+                <p className="font-semibold">Regular Price</p>
+                <span className="text-xs">(₹/Month)</span>
+              </div>
             </div>
             <div className="flex items-center gap-2">
               <input
                 type="number"
                 className="p-3 border border-gray-300 rounded-lg"
-                id="bedroom"
+                id="discountPrice"
                 min="1"
                 max="10"
                 required
               />
-              <p className="font-semibold">Beds</p>
+              <div className="flex flex-col items-center">
+                <p className="font-semibold">Discounted Price</p>
+                <span className="text-xs">(₹/Month)</span>
+              </div>
             </div>
           </div>
+        </div>
+        <div className="flex flex-col flex-1 gap-4">
+          <p>
+            Images:
+            <span className="font-normal text-gray-600 ml-2">
+              The First Image Will be in Cover (Max Image: 6)
+            </span>
+          </p>
+          <div className="flex gap-4">
+            <input
+              className="p-3 border border-gray-400 rounded w-full"
+              type="file"
+              id="images"
+              accept="image/*"
+              multiple
+            />
+            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+              Upload
+            </button>
+          </div>
+        <button className="p-3 bg-slate-800 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Create Listing</button>
         </div>
       </form>
     </main>
