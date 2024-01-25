abb59eec85bd17fafb7e9fe9c5f920691e00c0e4
nishanth-s03 <nishubangerasz@gmail.com>
2024-01-25 21:06:03 +0530

    Completed: Show listing backend!!!

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,54 +1,64 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const test = (req, res) => {
-  res
-  .json({ 
-    message: "User controller works!" 
+  res.json({
+    message: "User controller works!",
   });
 };
 
-export const updateUser = async (req,res,next) => {
-  if(req.user.id !== req.params.id) {
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
     return next(errorHandler(401, "You are not allowed to update this user"));
   }
   try {
-    if(req.body.password) {
+    if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
 
     const updatedUser = await User.findByIdAndUpdate(
-    req.params.id,
-    {
-      $set: {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        avatar: req.body.avatar,
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
       },
-    },{new:true}
+      { new: true }
     );
 
     const { password, ...rest } = updatedUser._doc;
-    res
-      .status(200)
-      .json(rest);
+    res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
 };
 
-export const deleteUser = async (req,res,next) => {
-  if(req.user.id !== req.params.id) return next(errorHandler(401, "You are not allowed to delete this user"));
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You are not allowed to delete this user"));
   try {
-    await User.findByIdAndDelete(req.params.id)
-    res.
-      clearCookie('access_token');
-    res
-      .status(200)
-      .json('User Has been Deleted!!');
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User Has been Deleted!!");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
