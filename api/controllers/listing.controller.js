e7fab6f89c7361a6ef1fd44a8c1613021320afeb
nishanth-s03 <nishubangerasz@gmail.com>
2024-01-26 11:59:42 +0530

    Created: Update Listing Routes and Controllers!!

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req,res,next) => {
     try {
@@ -29,4 +30,26 @@ export const deleteListing = async (req, res, next) => {
       next(error);
     }
   };
+
+  export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
+    }
+    if (req.user.id !== listing.userRef) {
+      return next(errorHandler(401, 'You can only update your own listings!'));
+    }
+  
+    try {
+      const updatedListing = await Listing.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      res.status(200).json(updatedListing);
+    } catch (error) {
+      next(error);
+    }
+  };
+  
   
