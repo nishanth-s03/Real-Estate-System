6159832a731abcaf914ca2651b7fa34f7e2d70e1
nishanth-s03 <nishubangerasz@gmail.com>
2024-01-26 14:59:04 +0530

    Completed: Contact Landlord Module!!!

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -11,7 +11,7 @@ export default function Contact({ listing }) {
   useEffect(() => {
     const fetchLandlord = async () => {
       try {
-        const res = await fetch(`/api/user/${listing.userRef}`);
+        const res = await fetch(`/api/users/${listing.userRef}`);
         const data = await res.json();
         setLandlord(data);
       } catch (error) {
